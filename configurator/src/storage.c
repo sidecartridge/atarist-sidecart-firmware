5ac2b70289107885b645719418ca5281ed0041af
Diego Parrilla <diego.parrilla.santamaria@gmail.com>
2024-10-31 16:52:11 +0100

    Do not display timeout message

--- configurator/src/storage.c
+++ configurator/src/storage.c
@@ -14,7 +14,8 @@ __uint16_t get_storage_status(__uint16_t show_bar)
     int err = send_sync_command(GET_SD_DATA, NULL, 0, STORAGE_WAIT_TIME, FALSE);
     if (err != 0)
     {
-        snprintf(buffer, STATUS_STRING_BUFFER_SIZE, "Cannot read microSD status. Is the device connected?");
+        // snprintf(buffer, STATUS_STRING_BUFFER_SIZE, "Cannot read microSD status. Retrying...");
+        return err;
     }
     else
     {
